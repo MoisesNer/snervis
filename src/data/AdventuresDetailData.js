81ab0ce9b1bc6e8ffeaefaaff4e029bb45b41b32
MoisesNer <StarCommand@Moisess-iMac.local>
2021-08-25 13:46:22 -0700

    added images

--- src/data/AdventuresDetailData.js
+++ src/data/AdventuresDetailData.js
@@ -1,6 +1,6 @@
-import about0 from '../assets/about0.png'
-import about1 from '../assets/about1.png'
-import about2 from '../assets/about2.png'
+// import about0 from '../assets/about0.png'
+// import about1 from '../assets/about1.png'
+// import about2 from '../assets/about2.png'
 
 import canada1 from '../assets/gallery/canada1.png'
 import canada2 from '../assets/gallery/canada2.png'
@@ -23,6 +23,71 @@ import chile5 from '../assets/gallery/chile5.png'
 import chile7 from '../assets/gallery/chile7.png'
 import chile8 from '../assets/gallery/chile8.png'
 import chile9 from '../assets/gallery/chile9.png'
+import france1 from '../assets/gallery/france1.png'
+import france2 from '../assets/gallery/france2.png'
+import france3 from '../assets/gallery/france3.png'
+import france4 from '../assets/gallery/france4.png'
+import france5 from '../assets/gallery/france5.png'
+import france6 from '../assets/gallery/france6.png'
+import france7 from '../assets/gallery/france7.png'
+import france8 from '../assets/gallery/france8.png'
+import italy1 from '../assets/gallery/italy1.png'
+import italy2 from '../assets/gallery/italy2.png'
+import italy3 from '../assets/gallery/italy3.png'
+import italy4 from '../assets/gallery/italy4.png'
+import italy5 from '../assets/gallery/italy5.png'
+import italy6 from '../assets/gallery/italy6.png'
+import italy7 from '../assets/gallery/italy7.png'
+import italy8 from '../assets/gallery/italy8.png'
+import italy9 from '../assets/gallery/italy9.png'
+import mexico0 from '../assets/gallery/mexico0.png'
+import mexico1 from '../assets/gallery/mexico1.png'
+import mexico3 from '../assets/gallery/mexico3.png'
+import mexico2 from '../assets/gallery/mexico2.png'
+import mexico4 from '../assets/gallery/mexico4.png'
+import mexico5 from '../assets/gallery/mexico5.png'
+import mexico6 from '../assets/gallery/mexico6.png'
+import mexico7 from '../assets/gallery/mexico7.png'
+import mexico8 from '../assets/gallery/mexico8.png'
+import mexico9 from '../assets/gallery/mexico9.png'
+import mexico10 from '../assets/gallery/mexico10.png'
+import mexico11 from '../assets/gallery/mexico11.png'
+import mexico12 from '../assets/gallery/mexico12.png'
+import mexico13 from '../assets/gallery/mexico13.png'
+import mexico14 from '../assets/gallery/mexico14.png'
+import mexico15 from '../assets/gallery/mexico15.png'
+import mexico16 from '../assets/gallery/mexico16.png'
+import mexico17 from '../assets/gallery/mexico17.png'
+import mexico18 from '../assets/gallery/mexico18.png'
+import mexico19 from '../assets/gallery/mexico19.png'
+import spain1 from '../assets/gallery/spain1.png'
+import spain2 from '../assets/gallery/spain2.png'
+import spain3 from '../assets/gallery/spain3.png'
+import spain4 from '../assets/gallery/spain4.png'
+import spain5 from '../assets/gallery/spain5.png'
+import spain6 from '../assets/gallery/spain6.png'
+import spain7 from '../assets/gallery/spain7.png'
+import spain8 from '../assets/gallery/spain8.png'
+import spain9 from '../assets/gallery/spain9.png'
+import spain10 from '../assets/gallery/spain10.png'
+import usa0 from '../assets/gallery/usa0.png'
+import usa1 from '../assets/gallery/usa1.png'
+import usa2 from '../assets/gallery/usa2.png'
+import usa3 from '../assets/gallery/usa3.png'
+import usa4 from '../assets/gallery/usa4.png'
+import usa5 from '../assets/gallery/usa5.png'
+import usa6 from '../assets/gallery/usa6.png'
+import usa7 from '../assets/gallery/usa7.png'
+import usa8 from '../assets/gallery/usa8.png'
+import usa9 from '../assets/gallery/usa9.png'
+import usa10 from '../assets/gallery/usa10.png'
+import usa11 from '../assets/gallery/usa11.png'
+import usa12 from '../assets/gallery/usa12.png'
+import usa13 from '../assets/gallery/usa13.png'
+import usa14 from '../assets/gallery/usa14.png'
+import usa15 from '../assets/gallery/usa15.png'
+
+
 
 export const AdventureDetails = [
     {
@@ -32,9 +97,15 @@ export const AdventureDetails = [
         heading: 'Travelling through the Mediterranean Sea, from a cruise, disembarked in ITALY Nov,2018',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         images:[   
-            about0,         
-            about1,         
-            about2,         
+            italy1,                  
+            italy2,                  
+            italy3,                  
+            italy4,                  
+            italy5,                  
+            italy6,                  
+            italy7,                  
+            italy8,                  
+            italy9,                  
         ] 
 
     },
@@ -45,11 +116,16 @@ export const AdventureDetails = [
         heading: 'SEBI goes to visit his grand grandmothers in SPAIN, Nov 2018',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         images:[    
-            chile4,         
-            chile5,         
-            chile7,         
-            chile8,         
-            chile9,          
+            spain1,         
+            spain2,         
+            spain3,         
+            spain4,         
+            spain5,         
+            spain6,         
+            spain7,         
+            spain8,         
+            spain9,         
+            spain10,         
         ] 
 
     },
@@ -78,12 +154,26 @@ export const AdventureDetails = [
         heading: 'Living his first year in town visits Val`Quirico Puebla MEXICO, July 2018',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         images:[    
-            canada3,         
-            canada4,         
-            canada6,         
-            canada7,         
-            canada8,         
-            canada9,                 
+            mexico0,                 
+            mexico1,                 
+            mexico2,                 
+            mexico3,                 
+            mexico4,                 
+            mexico5,                 
+            mexico6,                 
+            mexico7,                 
+            mexico8,                 
+            mexico9,                 
+            mexico10,                 
+            mexico11,                 
+            mexico12,                 
+            mexico13,                 
+            mexico14,                 
+            mexico15,                 
+            mexico16,                 
+            mexico17,                 
+            mexico18,                 
+            mexico19,                                 
         ] 
 
     },
@@ -94,10 +184,14 @@ export const AdventureDetails = [
         heading: 'Travelling through the Mediterranean Sea, from a cruise, disembarked in FRANCE, Nov 2018',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         images:[  
-            canada6,         
-            canada4,         
-            canada3,         
-            canada7,           
+            france1,           
+            france2,           
+            france3,           
+            france4,           
+            france5,           
+            france6,           
+            france7,           
+            france8,           
         ] 
 
     },
@@ -114,10 +208,8 @@ export const AdventureDetails = [
             canada4,         
             canada6,         
             canada7,         
-            canada8,         
-            canada9,         
         ] 
-
+        
     },
     {
         title: 'Burnaby',
@@ -126,11 +218,39 @@ export const AdventureDetails = [
         heading: 'Hiking is his passion, visiting the three mountains that surrounds Vancouver, Winter 2020',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         images:[    
+            canada8,         
+            canada9,         
             canada10,
             canada11,
             canada12,
             canada13,
             canada14        
+        ] 
+
+    },
+    {
+        title: 'Orlando',
+        slug: 'orlando',
+        label: 'international',
+        heading: 'A must and at the same time a dream for kids of all ages, Orlando and Disney May 2019',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        images:[    
+            usa0,
+            usa1,
+            usa2,
+            usa3,
+            usa4,
+            usa5,
+            usa6,
+            usa7,
+            usa8,
+            usa9,
+            usa10,
+            usa11,
+            usa12,
+            usa13,
+            usa14,
+            usa15,
         ] 
 
     },
